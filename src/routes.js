511967002e3bac7ec2428a8650c9967b801f2eea
ItaloBasilio <italobasiliodasilva@gmail.com>
2023-12-18 05:14:12 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-    return response.json({ message: "Hello World"})
+routes.get('/', async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'Italo',
+        email: 'italobasilio2@email.com',
+        password_hash: '23asdf234',
+        admin: true,
+    })
+    return response.json(user)
 })
 
 export default routes
